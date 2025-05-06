4387a8ae6b6213d3f855a140e284005e37f7a4db
Nate <kristophernhamm@gmail.com>
2025-05-05 23:38:11 -0500

    Fix: remove vite-tsconfig-paths, use manual alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 export default defineConfig({
   base: '/',
-  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [react()],
 })
